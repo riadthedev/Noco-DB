ac68c25f2f07464ed1f480a7d5c0026b0c9d2127
John Tobi <johnriad.dev@gmail.com>
2024-09-27 15:26:00 -0300

    Security Update.

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON requests
+const app = express();
+const PORT = process.env.PORT;
+
 app.use(express.json());
 
 // Endpoint to receive POST requests from NocoDB
 app.post('/', async (req, res) => {
-    const {Email} = req.body.data; // Adjust this according to your NocoDB payload
+    const {Email} = req.body.data; 
 
     // Construct the message to send to Discord
     const discordMessage = `New review requested for ${Email}`;
 
     try {
-        await axios.post('https://discord.com/api/webhooks/1289258497439039489/p9DKCm2zDbshHp3hoEGaG2OGo-9lDBvGCGJ3z-nOzZ9Do2-43ruTUY_LvyRlBujsAyYA', { content: discordMessage });
+        await axios.post(process.env.DISCORD_WEBHOOK_URL, { content: discordMessage });
         res.status(200).send('Message sent to Discord');
     } catch (error) {
         console.error('Error sending message to Discord:', error);
         res.status(500).send('Error sending message');
     }
 });
-// Enhanced health check endpoint
 app.get('/health', async (req, res) => {
     try {
         // Perform a simple POST request to the Discord webhook
